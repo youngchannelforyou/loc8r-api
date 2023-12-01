2e618a20d3887da511ddf06ea09fd168892b8b3a
uchanyoung <pbl3_t5>
2023-12-01 21:56:20 +0900

    abc

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/models/db');
+const passport = require('passport');
+require('./app_api/models/db');
+require('./app_api/config/passport');
+require('dotenv').config()
 
 const indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
@@ -14,14 +17,42 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, \
+  Content-Type, Accept, Authorization');
+  next();
+})
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+app.use(passport.initialize());
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ "message": err.name + ": " + err.message });
+  }
+})
+
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+
+
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+})
+
+app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.htmI'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,5 +69,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
